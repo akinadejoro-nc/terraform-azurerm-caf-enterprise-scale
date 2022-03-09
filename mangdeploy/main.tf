bf2062b0c042bd793729edcb0742668cdb4617b6
akinadejoro-nc <akinyemi.adejoro@nordcloud.com>
2022-03-09 18:43:46 +0000

    mangdeploy added

---
+++ mangdeploy/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.77.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# You can use the azurerm_client_config data resource to dynamically
+# extract connection settings from the provider configuration.
+
+# data "azurerm_client_config" "core" {}
+
+# Call the caf-enterprise-scale module directly from the Terraform Registry
+# pinning to the latest version
+
+module "enterprise_scale" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "1.1.2"
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
+
+  root_parent_id = data.azurerm_client_config.core.tenant_id
+#   root_parent_id = "e832a371-219a-4da3-bee2-0cddcbd922bd"
+  root_id        = "my"
+  root_name      = "My Organization"
+
+}
